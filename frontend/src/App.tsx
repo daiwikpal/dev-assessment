829e9fd1ea098dd3574eefef7aaef4a6eb996b16
Daiwik Pal <daiwikpal123@gmail.com>
2023-08-29 22:32:09 -0400

    updated ReadMe

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,13 @@ import UserTable from "./components/UserTable";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 
+// Create a nav bar where people can enter a new user 
 
 function App() {
   return (
     <div>
       <TopBar />
+      <UserTable />
     </div>
   )
 }
--- frontend/src/components/UserTable.tsx
+++ frontend/src/components/UserTable.tsx
@@ -48,7 +48,6 @@ function UserTable() {
           })}
         </tbody>
       </table>
-      <PaginationRow />
     </div>
   );
 }
